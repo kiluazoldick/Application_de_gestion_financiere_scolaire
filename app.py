c1a1a030dd0895b0889e8a15c38d6860357cea7e
Kilua Zoldick <nangadoumer@gmail.com>
2025-05-31 02:09:00 +0100

    optimisation de la bd ave supabase

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import streamlit as st
 from db import init_db
 init_db()
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # charger les variables d'environnement depuis .env
+
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+# initialisation Supabase
+from supabase import create_client, Client
+
+supabase: Client = create_client(supabase_url, supabase_key)
 
 st.set_page_config(page_title="L'avenir du peuple Gestion Fianciere", layout="wide")
 init_db()
